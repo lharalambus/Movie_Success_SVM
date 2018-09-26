256dc222b715f1100cc8532092849f27b2773a3d
dsteel12 <43549446+dsteel12@users.noreply.github.com>
2018-09-26 12:15:56 -0400

    Added new Data Collector class

---
+++ DataCollector.java
@@ -0,0 +1,26 @@
+public class DataCollector {
+
+    //Twitter data collector - Ubeyd
+    public static void TwitterCollector(){
+
+    }
+
+    //IMDB data collector - Yekta
+    public static void IMDBCollector(){
+
+    }
+
+    //Rotten Tomatoes data collector - Leon
+    public static void RTCollector(){
+
+    }
+
+    //YouTube data collector - Derek
+    public static void YouTubeCollector(){
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
